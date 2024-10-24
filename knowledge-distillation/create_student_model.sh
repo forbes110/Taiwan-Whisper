8ca9130fbef747be505cda9a719c7f5c4062dc5d
forbes110 <seer0970202206@gmail.com>
2024-10-24 12:10:22 +0800

    done

--- knowledge-distillation/create_student_model.sh
+++ knowledge-distillation/create_student_model.sh
@@ -1,4 +1,4 @@
-# # init
+# init
 # python create_student_model.py \
 #   --teacher_checkpoint "openai/whisper-large-v2" \
 #   --encoder_layers 32 \
@@ -6,10 +6,10 @@
 #   --save_dir "/mnt/student_model"\
 #   --mix_lang_emb
 
-# init
+# for check
 python create_student_model.py \
   --teacher_checkpoint "openai/whisper-tiny" \
-  --encoder_layers 2 \
+  --encoder_layers 1 \
   --decoder_layers 2 \
   --save_dir "/mnt/student_model"\
   --mix_lang_emb
